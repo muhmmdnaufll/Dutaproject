de05ca7eb2c38d48451049f92bf5d04349021537
muhmmdnaufll <naufalm732@gmail.com>
2026-05-07 01:45:31 +0700

    Add clean bundled app entry

---
+++ src/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles.css';
+import './responsive.css';
+import './footer.css';
+
+createRoot(document.getElementById('root')).render(<App />);
