adb6b6cbe7162c906471d1e4dd4f3b0c32bd0acf
muhmmdnaufll <naufalm732@gmail.com>
2026-05-07 01:53:44 +0700

    Load final mobile polish stylesheet

--- src/entry.jsx
+++ src/entry.jsx
@@ -4,5 +4,6 @@ import App from './App.jsx';
 import './styles.css';
 import './responsive.css';
 import './footer.css';
+import './mobile.css';
 
 createRoot(document.getElementById('root')).render(<App />);
