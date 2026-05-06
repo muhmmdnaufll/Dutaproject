c2fb2b3969a577c7922bc55a027515bd3fb18dd8
muhmmdnaufll <naufalm732@gmail.com>
2026-05-07 02:03:04 +0700

    Remove unused entry file

--- src/entry.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
-import './responsive.css';
-import './footer.css';
-import './mobile.css';
-
-createRoot(document.getElementById('root')).render(<App />);
